6774d980b3cf1ee38fdd8f09cac85347406bf613
Brandon Castro <castroyou999@gmail.com>
2022-09-13 21:17:15 -0400

    commit tree 9e28e29ce01781350d4ea4d4c3aabb7bfac5799c

--- index.js
+++ index.js
@@ -11,6 +11,16 @@ const tutorials = [
   'what is JSONP?'
 ];
 
+// const titleCased = () => {
+//   return tutorials
+// }
+
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(tutorial => {
+      const words = tutorial.split(" ");
+      const capWords = words.map(word => word[0].toUpperCase() + word.slice(1));
+      const tutorialsCap = capWords.join(" ");
+      return tutorialsCap
+  })
 }
+
